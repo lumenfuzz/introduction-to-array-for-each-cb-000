dee28bcb2771fd0d0ab78a82d6f197fdeb8b4604
Patrick J Louis <lumenfuzz@gmail.com>
2018-04-29 20:43:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Add your doToElementsInArray() function here:
 function doToElementsInArray(array, callback) {
-  
+
 }
 
 // Add your changeCompletely() function here:
 function changeCompletely() {
-  
+
 }
